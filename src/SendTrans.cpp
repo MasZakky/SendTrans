6fd4ebc43dc81056634aee435b07f575e96c9ee7
Mas Zakky <44503482+MasZakky@users.noreply.github.com>
2019-04-17 01:31:16 +0700

    .
    .

--- src/SendTrans.cpp
+++ src/SendTrans.cpp
@@ -38,11 +38,31 @@ int8_t SendTransI2C::setWrite(int8_t val){
   }
 }
 
-int8_t SendTransI2C::setWrite(int8_t *val,int8_t val2){
+int8_t SendTransI2C::setWrite(int8_t val,int8_t val2){
   if(_useWire == Use_Default){
     set_Wire.beginTransmission(_Address);
-    for(int8_t asa = 0; asa <= val2; asa++){
-      set_Wire.set_Write(val[asa]);
+    set_Wire.set_Write(val);
+    set_Wire.set_Write(val2);
+    int8_t value = set_Wire.endTransmission();
+    if(value > 0) return Process_ERROR;
+    else return Process_OK;
+  }
+  else if(_useWire == Use_External){
+    ExWire.beginTransmission(_Address);
+    ExWire.set_Write(val);
+    ExWire.set_Write(val2);
+    int8_t value = ExWire.endTransmission();
+    if(value > 0) return Process_ERROR;
+    else return Process_OK;
+  }
+}
+
+int8_t SendTransI2C::setWrite(int8_t val,int8_t *val2,int8_t val3){
+  if(_useWire == Use_Default){
+    set_Wire.beginTransmission(_Address);
+    set_Wire.set_Write(val);
+    for(int8_t asa = 0; asa <= val3; asa++){
+      set_Wire.set_Write(val2[asa]);
     }
     int8_t value = set_Wire.endTransmission();
     if(value > 0) return Process_ERROR;
@@ -50,8 +70,9 @@ int8_t SendTransI2C::setWrite(int8_t *val,int8_t val2){
   }
   else if(_useWire == Use_External){
     ExWire.beginTransmission(_Address);
-    for(int8_t asa = 0; asa <= val2; asa++){
-      ExWire.set_Write(val[asa]);
+    ExWire.set_Write(val);
+    for(int8_t asa = 0; asa <= val3; asa++){
+      ExWire.set_Write(val2[asa]);
     }
     int8_t value = ExWire.endTransmission();
     if(value > 0) return Process_ERROR;
