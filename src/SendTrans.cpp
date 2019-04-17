b6444cdcb69324797c9fc63ee88087234eb37d81
Mas Zakky <44503482+MasZakky@users.noreply.github.com>
2019-04-18 02:07:37 +0700

    .
    .

--- src/SendTrans.cpp
+++ src/SendTrans.cpp
@@ -3,76 +3,89 @@
 int8_t SendTransI2C::setWire(int8_t val){
   return  SendTransI2C::setWire(val, 0x00);
 }
-
 int8_t SendTransI2C::setWire(int8_t val, byte Address){
   _useWire = (byte)val | (Address<<1);
   return Process_OK;
 }
 
-#ifdef EX_Tiny
-int8_t SendTransI2C::InstalWire(USI_TWI &val){
-  ExWire *= &val;
+int8_t SendTransI2C::InstalWire(TwoWire &val){
+  ExWire = val;
   return Process_OK;
 }
-#else  
-int8_t SendTransI2C::InstalWire(Stream &val){
-  ExWire *= &val;
-  return Process_OK;
-}
-#endif
 
 int8_t SendTransI2C::setWrite(int8_t val){
   if(_useWire == Use_Default){
-    set_Wire.beginTransmission(_Address);
-    set_Wire.set_Write(val);
-    int8_t value = set_Wire.endTransmission();
+#ifdef EX_Tiny
+    TinyWireM.beginTransmission(_Address);
+    TinyWireM.send(val);
+    int8_t value = TinyWireM.endTransmission();
+#else
+    Wire.beginTransmission(_Address);
+    Wire.write(val);
+    int8_t value = Wire.endTransmission();
+#endif
     if(value > 0) return Process_ERROR;
     else return Process_OK;
   }
   else if(_useWire == Use_External){
     ExWire.beginTransmission(_Address);
-    ExWire.set_Write(val);
+    ExWire.write(val);
     int8_t value = ExWire.endTransmission();
     if(value > 0) return Process_ERROR;
     else return Process_OK;
   }
 }
-
 int8_t SendTransI2C::setWrite(int8_t val,int8_t val2){
   if(_useWire == Use_Default){
-    set_Wire.beginTransmission(_Address);
-    set_Wire.set_Write(val);
-    set_Wire.set_Write(val2);
-    int8_t value = set_Wire.endTransmission();
+#ifdef EX_Tiny
+    TinyWireM.beginTransmission(_Address);
+    TinyWireM.send(val);
+    TinyWireM.send(val2);
+    int8_t value = TinyWireM.endTransmission();
+#else
+    Wire.beginTransmission(_Address);
+    Wire.write(val);
+    Wire.write(val2);
+    int8_t value = Wire.endTransmission();
+#endif
     if(value > 0) return Process_ERROR;
     else return Process_OK;
   }
   else if(_useWire == Use_External){
     ExWire.beginTransmission(_Address);
-    ExWire.set_Write(val);
-    ExWire.set_Write(val2);
+    ExWire.write(val);
+    ExWire.write(val2);
     int8_t value = ExWire.endTransmission();
     if(value > 0) return Process_ERROR;
     else return Process_OK;
   }
 }
-
 int8_t SendTransI2C::setWrite(int8_t val,int8_t *val2,int8_t val3){
+  if(val3<=0) return Process_ERROR;
   if(_useWire == Use_Default){
-    set_Wire.beginTransmission(_Address);
-    set_Wire.set_Write(val);
+#ifdef EX_Tiny
+    TinyWireM.beginTransmission(_Address);
+    TinyWireM.write(val);
     for(int8_t asa = 0; asa <= val3; asa++){
-      set_Wire.set_Write(val2[asa]);
+      TinyWireM.write(val2[asa]);
     }
-    int8_t value = set_Wire.endTransmission();
+    int8_t value = TinyWireM.endTransmission();
+#else
+    Wire.beginTransmission(_Address);
+    Wire.write(val);
+    for(int8_t asa = 0; asa <= val3; asa++){
+      Wire.write(val2[asa]);
+    }
+    int8_t value = Wire.endTransmission();
+#endif
     if(value > 0) return Process_ERROR;
     else return Process_OK;
   }
   else if(_useWire == Use_External){
     ExWire.beginTransmission(_Address);
-    ExWire.set_Write(val);
+    ExWire.write(val);
     for(int8_t asa = 0; asa <= val3; asa++){
-      ExWire.set_Write(val2[asa]);
+      ExWire.write(val2[asa]);
     }
     int8_t value = ExWire.endTransmission();
     if(value > 0) return Process_ERROR;
@@ -80,57 +93,83 @@ int8_t SendTransI2C::setWrite(int8_t val,int8_t *val2,int8_t val3){
   }
 }
 
-int8_t SendTransI2C::getRead(int8_t val,int8_t &val2){
+int8_t SendTransI2C::getRead(int8_t val,uint8_t& val2){
   if(_useWire == Use_Default){
-    set_Wire.beginTransmission(_Address);
-    set_Wire.set_Write(val);
-    int8_t value = set_Wire.endTransmission();
+#ifdef EX_Tiny
+    TinyWireM.beginTransmission(_Address);
+    TinyWireM.send(val);
+    int8_t value = TinyWireM.endTransmission();
     if(value > 0) return Process_ERROR;
 
-    set_Wire.requestFrom(_Address,1);
-    val2 = set_Wire.get_Read();
-    
+    TinyWireM.requestFrom(_Address,1);
+    if(TinyWireM.available()) val2 = (uint8_t)TinyWireM.receive();
+#else
+    Wire.beginTransmission(_Address);
+    Wire.write(val);
+    int8_t value = Wire.endTransmission();
+    if(value > 0) return Process_ERROR;
+
+    Wire.requestFrom(_Address,1);
+    if(Wire.available()) val2 = (uint8_t)Wire.read();
+#endif
     return Process_OK;
   }
   else if(_useWire == Use_External){
     ExWire.beginTransmission(_Address);
-    ExWire.set_Write(val);
+    ExWire.write(val);
     int8_t value = ExWire.endTransmission();
     if(value > 0) return Process_ERROR;
 
     ExWire.requestFrom(_Address,1);
-    val2 = ExWire.get_Read();
-    
+    if(ExWire.available()) val2 = (uint8_t)ExWire.read();
+
     return Process_OK;
   } 
 }
-
-int8_t SendTransI2C::getRead(int8_t val,int8_t &val2,int8_t val3){
+int8_t SendTransI2C::getRead(int8_t val,uint8_t& val2,uint8_t val3){
+  if(val <= 0) return Process_ERROR;
   if(_useWire == Use_Default){
-    set_Wire.beginTransmission(_Address);
-    set_Wire.set_Write(val);
-    int8_t value = set_Wire.endTransmission();
+#ifdef EX_Tiny
+    TinyWireM.beginTransmission(_Address);
+    TinyWireM.send(val);
+    int8_t value = TinyWireM.endTransmission();
+    if(value > 0) return Process_ERROR;
+    
+    TinyWireM.requestFrom(_Address,val3);
+    if(TinyWireM.available() == val3){
+      for(uint8_t asa = 0; asa <= val2; asa++){
+        val2[asa] = (uint8_t)TinyWireM.receive();
+      }
+    }
+#else
+    Wire.beginTransmission(_Address);
+    Wire.write(val);
+    int8_t value = Wire.endTransmission();
     if(value > 0) return Process_ERROR;
 
-    set_Wire.requestFrom(_Address,val3);
-    for(int8_t asa = 0; asa <= val2; asa++){
-      val2[asa] = set_Wire.get_Read();
+    Wire.requestFrom(_Address,val3);
+    if(Wire.available() == val3){ 
+      for(uint8_t asa = 0; asa <= val2; asa++){
+        val2[asa] = (uint8_t)Wire.read();
+      }
     }
-    
+#endif
     return Process_OK;
   }
   else if(_useWire == Use_External){
     ExWire.beginTransmission(_Address);
-    ExWire.set_Write(val);
+    ExWire.write(val);
     int8_t value = ExWire.endTransmission();
     if(value > 0) return Process_ERROR;
-
-    ExWire.requestFrom(_Address,val3);
-    for(int8_t asa = 0; asa <= val2; asa++){
-      val2[asa] = ExWire.get_Read();
-    }
     
+    ExWire.requestFrom(_Address,val3);
+    if(ExWire.available() == val3){ 
+      for(uint8_t asa = 0; asa <= val3; asa++){
+        val2[asa] = (uint8_t)ExWire.read();
+      }
+    } 
     return Process_OK;
   }
 }
-  
+
+//extern SendTransI2C SimpleWire;
