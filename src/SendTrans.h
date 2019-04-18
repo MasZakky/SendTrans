90a5289c588a24872028324f8623fc9df970aea8
Mas Zakky <44503482+MasZakky@users.noreply.github.com>
2019-04-18 18:17:14 +0700

    Update
    Add I2C MultiPlexer

--- src/SendTrans.h
+++ src/SendTrans.h
@@ -18,13 +18,13 @@
       #define EX_Tiny 
 #endif
 
-// #define Not_UseSwitch 0
-// #define Yes_UseSwitch 1
+#define Not_UseSwitch 0
+#define Yes_UseSwitch 1
 
-// #define Yes_UseExpander 0
-// #define Not_UseExpander 1
+#define Yes_UseExpander 0
+#define Not_UseExpander 1
 
-// #define Use_TCA9548A 0
+#define Use_TCA9548A 0
 
 #define Process_Not_Found -1
 #define Process_OK         0 
@@ -34,37 +34,37 @@ class SendTransI2C{
   public: `
     int8_t setWire(byte Address);
     int8_t setWire(byte val,byte val2,byte val3,byte val4,byte val5,byte val6,byte val7);
-//     int8_t setClock(uint16_t val);
-//     int8_t setMultiPlexer(int8_t val = Not_UseSwitch,int8_t val2 = Use_TCA9548A);
-//     int8_t setChannel(int8_t val);
-    
-#ifdef ARDUINO_SAM_DUE
-    int8_t InstalWire(TwoWire &val = Wire1);
-#elif defined(EX_Tiny)
-    int8_t InstalWire();
-#else  
-    int8_t InstalWire(TwoWire &val = Wire);
+ 
+    int8_t setMultiPlexer(int8_t val = Not_UseSwitch,int8_t val2 = Use_TCA9548A);
+    int8_t setAddressMultiPlexer(byte Address = _AddressTCA9548A,byte val1,byte val2,byte val3,byte val4);
+    int8_t setChannel(int8_t val);
+
+#if !defined EX_Tiny    
+    #ifdef ARDUINO_SAM_DUE
+        int8_t InstalWire(TwoWire &val = Wire1);
+    #else  
+        int8_t InstalWire(TwoWire &val = Wire);
+    #endif
 #endif
     
     int8_t setWrite(int8_t val);
     int8_t setWrite(int8_t val,int8_t val2);
     int8_t setWrite(int8_t val,int8_t *val2,int8_t val3);
+ 
 
-
-    int8_t getRead(int8_t val,uint8_t& val2);
-    int8_t getRead(int8_t val,uint8_t& val2,uint8_t val3);
+    int8_t getRead(int val,int& val2);
+    int8_t getRead(int val,int& val2,int val3);
   
   protected:
-//     int8_t AutoSwitchChannel();
+    int8_t AutoSwitchChannel();
     
   #if !defined(EX_Tiny)
     TwoWire ExWire;  
   #endif
-    byte _Address;
-//     uint16_t _Clock;
-//     int8_t _useSwitch;
-//     int8_t _nameIC;
-//     int8_t _setChannel;
+    byte _Address,_Address2;
+    int8_t _useSwitch;
+    int8_t _nameIC;
+    int8_t _setChannel;
 };
 //extern SendTransI2C SimpleWire;
 #endif
