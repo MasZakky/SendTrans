14da10080fc04e5d2d3caa57c6ce7ba5eb34ae8f
Mas Zakky <44503482+MasZakky@users.noreply.github.com>
2019-04-18 18:29:16 +0700

    Update
    Add I2C MultiPlexer

--- src/SendTrans.h
+++ src/SendTrans.h
@@ -26,6 +26,8 @@
 
 #define Use_TCA9548A 0
 
+#define _AddressTCA9548A 0x70
+
 #define Process_Not_Found -1
 #define Process_OK         0 
 #define Process_ERROR      1
@@ -35,10 +37,12 @@ class SendTransI2C{
     int8_t setWire(byte Address);
     int8_t setWire(byte val,byte val2,byte val3,byte val4,byte val5,byte val6,byte val7);
  
+    //I2C MultiPlexer
     int8_t setMultiPlexer(int8_t val = Not_UseSwitch,int8_t val2 = Use_TCA9548A);
-    int8_t setAddressMultiPlexer(byte Address = _AddressTCA9548A,byte val1,byte val2,byte val3,byte val4);
+    int8_t setAddressMultiPlexer(byte Address = _AddressTCA9548A,byte val1 = 0,byte val2 = 0,byte val3 = 0,byte val4 = 0);
     int8_t setChannel(int8_t val);
-
+    
+    // Wire or Wire1 or Wire3 ~~
 #if !defined EX_Tiny    
     #ifdef ARDUINO_SAM_DUE
         int8_t InstalWire(TwoWire *val = Wire1);
@@ -47,19 +51,21 @@ class SendTransI2C{
     #endif
 #endif
     
+    //Write
     int8_t setWrite(int8_t val);
     int8_t setWrite(int8_t val,int8_t val2);
     int8_t setWrite(int8_t val,int8_t *val2,int8_t val3);
  
+    //read
     int8_t getRead(int val,int& val2);
     int8_t getRead(int val,int& val2,int val3);
   
   protected:
     int8_t AutoSwitchChannel();
     
-  #if !defined(EX_Tiny)
+#if !defined(EX_Tiny)
     TwoWire ExWire;  
-  #endif
+#endif
     byte _Address,_Address2;
     int8_t _useSwitch;
     int8_t _nameIC;
