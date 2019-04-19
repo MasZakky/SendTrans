8ff3f99058b2b83b33318ba941075c84861501ea
Mas Zakky <44503482+MasZakky@users.noreply.github.com>
2019-04-19 14:11:34 +0700

    Update

--- example/Read/Read_Single_Data.ino
+++ example/Read/Read_Single_Data.ino
@@ -3,14 +3,14 @@ SendTransI2C SimpleWire;
 
 void setup() {
   Wire.begin(); // join i2c bus (address optional for master)
-  SimpleWire.setWire(0x2C); // Address
+  SimpleWire.setWire(0x00); // Address
   Serial.begin(9600);
 }
 
 void loop() {
   Wire.setClock(100000); // setClock I2C 100kHZ
 
-  char c = Serial.read()    
+  int c = Serial.read()    
   SimpleWire.setWrite(SubbAddress, c); 
   
   delay(500);
